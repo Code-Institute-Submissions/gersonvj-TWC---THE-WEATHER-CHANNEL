81cced371a401687e93695ee7d7e59834858d0db
gersonvj <gersonvj@hotmail.com>
2020-11-04 22:37:26 +0000

    bringind data to variants

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,15 +1,27 @@
 const settings = {
-    "async": true,
-    "crossDomain": true,
-	"url": "https://rapidapi.p.rapidapi.com/weather?q=London%2Cuk&&units=metric&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html",
-    
-    "method": "GET",
+	"async": true,
+	"crossDomain": true,
+    "url": "https://rapidapi.p.rapidapi.com/forecast.json?q=London&days=3",
+    "contentType": "application/json",
+    "dataType": 'json',
+	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "fb5430219bmshd4a41358e678d75p187a51jsn30d831ea960f",
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+		"x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
 	}
 };
 
+
+dataType: 'json'
+
 $.ajax(settings).done(function (response) {
     console.log(response);
+    var currentWeatherCelsos = response.current.temp_c;
+   console.log(currentWeatherCelsos);
+
  });
+
+
+
+
+
