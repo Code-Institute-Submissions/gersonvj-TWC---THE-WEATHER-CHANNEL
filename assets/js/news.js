2efd2c71b74366dbfc864ea205e268fd81e9c8f7
gersonvj <gersonvj@hotmail.com>
2020-11-09 13:56:55 +0000

    news.html conected with news api

--- assets/js/news.js
+++ assets/js/news.js
@@ -16,19 +16,49 @@ $.ajax(settings).done(function (response) {
     var firstnews = response.articles[0].title;
     var secnews = response.articles[0].summary;
     var linknews = response.articles[0].link;
+    var secnews2 = response.articles[1].title;
+    var secnews2a = response.articles[1].summary;
+    var secnews2b = response.articles[1].link;
+    var thirdnews3 = response.articles[2].title;
+    var thirdnews3a = response.articles[2].summary;
+    var thirdnews3b = response.articles[2].link;
     
-    console.log(firstnews);
-    
+    //first api news
     let news1 = document.querySelector(".news1");
     news1.textContent = firstnews;
 
     let news1a = document.querySelector(".news1a");
     news1a.textContent = secnews;
 
-   let news1b = document.querySelector(".news-api-link");
-news1b.textContent = linknews;
+    let news1b = document.querySelector(".news-api-link");
+    news1b.textContent = linknews;
 
-document.getElementById("news-api-link").href=  linknews;
+    document.getElementById("news-api-link").href=  linknews;
+    
+
+    //second api news
+    let news2 = document.querySelector(".news2");
+    news2.textContent = secnews2;
+
+    let news2a = document.querySelector(".news2a");
+    news2a.textContent = secnews2a;
+
+    let news2b = document.querySelector(".news2b");
+    news2b.textContent = secnews2b;
+
+    document.getElementById("news2b").href=  secnews2b;
+
+    //third api news
+    let news3 = document.querySelector(".news3");
+    news3.textContent = thirdnews3b;
+
+    let news3a = document.querySelector(".news3a");
+    news3a.textContent = thirdnews3a;
+
+    let news3b = document.querySelector(".news3b");
+    news3b.textContent = thirdnews3b;
+
+    document.getElementById("news3b").href=  thirdnews3b;
 
    
 });
